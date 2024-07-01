7b83cd60f8220fb80996988ed0ef23286ccbe480
Szabolcs Toth <tsz@purzelbaum.hu>
2024-07-01 11:35:45 +0200

    Test was added.

--- SQLite/ParksOfPrague/Tests/AppTests/AppTests.swift
+++ SQLite/ParksOfPrague/Tests/AppTests/AppTests.swift
@@ -11,13 +11,25 @@ final class AppTests: XCTestCase {
         let port = 0
         let logLevel: Logger.Level? = .trace
     }
-
+    
     func testApp() async throws {
         let args = TestArguments()
-        let app = buildApplication(args)
+        let app = try await buildApplication(args)
         try await app.test(.router) { client in
             try await client.execute(uri: "/health", method: .get) { response in
                 XCTAssertEqual(response.status, .ok)
+            }
+        }
+    }
+    
+    func test_list_all_parks_on_empty_db() async throws {
+        let args = TestArguments()
+        let app = try await buildApplication(args)
+        try await app.test(.router) { client in
+            try await client.execute(uri: "/api/v1/parks", method: .get) { response in
+                let parks = try JSONDecoder().decode([Park].self, from: response.body)
+                
+                XCTAssertTrue(parks.isEmpty, "The parks array is not empty")
             }
         }
     }
